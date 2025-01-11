7e48b9a00137348703579066f34e5a8a3ad528f8
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-01-11 15:32:17 +0200

    Project 4 created

--- src/gr/aueb/cf/Project4.java
+++ src/gr/aueb/cf/Project4.java
@@ -9,17 +9,30 @@ public class Project4 {
 
     public static void main(String[] args) {
 
-         initializeBoard();
+        initializeBoard();
         int currentPlayer = 1;
+
 
         while (true){
 
             displayBoard();
             System.out.println("Player " + (currentPlayer == 1 ? "X" : "O") + " it's your turn");
 
-            int[] move = movePlayer();
+            int[] move = movePlayer(currentPlayer);
             board[move[0]][move[1]] = currentPlayer;
 
+
+            if(checkWinner(currentPlayer)) {
+                displayBoard();
+                System.out.println("Player " + (currentPlayer == 1 ? "X" : "0") + " wins!");
+                break;
+            }
+
+            if(isBoardFull()) {
+                displayBoard();
+                System.out.println(" It's a tie");
+                break;
+            }
             currentPlayer = (currentPlayer == 1) ? 2 : 1;
         }
 
@@ -60,30 +73,58 @@ public class Project4 {
     }
 
     public static boolean isMoveValid(int row, int col){
+        return !(row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != 0);
+    }
 
-        if(row < 0 || row > 2 || col < 0 || col > 2) {
-            return false;
+    public static int[] movePlayer(int currentPlayer) {
+        int row, col;
+
+        while (true) {
+
+            System.out.println("Enter row 0-2): ");
+            row = in.nextInt();
+
+            System.out.println("Enter col 0-2): ");
+            col = in.nextInt();
+
+            if (isMoveValid(row, col)) {
+                board[row][col] = currentPlayer;
+                return new int[]{row, col};
+            } else {
+                System.out.println("Invalid move. Please try again");
+            }
         }
-        if(board[row][col] != 0) {
-            return false;
+    }
+
+    public static boolean checkWinner(int currentPlayer){
+        for(int i = 0; i < 3; i++) {
+            if(board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true;
+            }
+            for(int j = 0; j < 3; j++){
+                if(board[0][j] == currentPlayer && board[1][j] == currentPlayer && board[2][j] == currentPlayer){
+                    return true;
+                }
+                if(board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer){
+                    return true;
+                }
+                if(board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isBoardFull(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(board[j][i] == 0) {
+                    return false;
+                }
+            }
         }
         return true;
     }
 
-    public static int[] movePlayer() {
-        int row, col;
-
-        while (true) {
-            System.out.println("Enter your move (row and col 0-2): ");
-            row = in.nextInt();
-            col = in.nextInt();
-
-            if(isMoveValid(row, col)){
-                break;
-            }else {
-                System.out.println("Invalid move. Please try again");
-            }
-        }
-        return new int[]{col,row};
-    }
 }
